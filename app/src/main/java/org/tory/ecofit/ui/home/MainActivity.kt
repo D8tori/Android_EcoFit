81b25303ef23c45d771bb5153361a144b22ab587
Hyuna Kim <akxmcse@gmail.com>
2022-06-22 21:08:54 +0900

    [ADD] 프로젝트 기본 세팅

---
+++ app/src/main/java/org/tory/ecofit/ui/home/MainActivity.kt
@@ -0,0 +1,12 @@
+package org.tory.ecofit.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.tory.ecofit.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
