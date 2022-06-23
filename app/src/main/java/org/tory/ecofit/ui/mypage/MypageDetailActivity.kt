b53911eca5f8c766183db4c80eaee034c2aa6474
Hyuna Kim <akxmcse@gmail.com>
2022-06-24 00:06:34 +0900

    [FEAT] 마이페이지->참여내역 연결

---
+++ app/src/main/java/org/tory/ecofit/ui/mypage/MypageDetailActivity.kt
@@ -0,0 +1,12 @@
+package org.tory.ecofit.ui.mypage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.tory.ecofit.R
+
+class MypageDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mypage_detail)
+    }
+}
--- app/src/main/java/org/tory/ecofit/ui/mypage/MypageFragment.kt
+++ app/src/main/java/org/tory/ecofit/ui/mypage/MypageFragment.kt
@@ -1,20 +1,36 @@
 package org.tory.ecofit.ui.mypage
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import org.tory.ecofit.R
+import org.tory.ecofit.databinding.FragmentMypageBinding
+import org.tory.ecofit.ui.market.MarketUploadActivity
 
 class MypageFragment : Fragment() {
+    private lateinit var binding: FragmentMypageBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mypage, container, false)
+        binding = FragmentMypageBinding.inflate(layoutInflater)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initClickListener()
+    }
+
+    private fun initClickListener(){
+        binding.tvChallengeHistory.setOnClickListener{
+            val intent = Intent(context, MypageDetailActivity::class.java)
+            context?.startActivity(intent)
+        }
+    }
 }
