dd8e224b9798bfca451f1516920df89ab546f2a1
Hyuna Kim <akxmcse@gmail.com>
2022-06-23 19:50:47 +0900

    [FEAT] 상품 리스트 리사이클러뷰 구현

---
+++ app/src/main/java/org/tory/ecofit/data/local/MarketItemData.kt
@@ -0,0 +1,7 @@
+package org.tory.ecofit.data.local
+
+data class MarketItemData(
+    val img: Int,
+    val price: Int,
+    val title: String
+)
--- app/src/main/java/org/tory/ecofit/ui/market/MarketFragment.kt
+++ app/src/main/java/org/tory/ecofit/ui/market/MarketFragment.kt
@@ -7,23 +7,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.GridLayoutManager
 import org.tory.ecofit.R
 import org.tory.ecofit.databinding.FragmentMarketBinding
 
 class MarketFragment : Fragment() {
     private lateinit var binding: FragmentMarketBinding
+    private val viewModel: MarketViewModel by activityViewModels()
+    private lateinit var marketListAdapter: MarketAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentMarketBinding.inflate(inflater, container, false)
+        binding = FragmentMarketBinding.inflate(layoutInflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        viewModel.setItemList()
+        setMarketAdapter()
+        setListObserver()
         setSubFood()
         setUpCycling()
         setLocalFood()
@@ -58,6 +66,21 @@ class MarketFragment : Fragment() {
     private fun setEtc(){
         binding.clTagEtc.setOnClickListener{
             binding.clTagEtc.isSelected = binding.clTagEtc.isSelected != true
+        }
+    }
+
+    private fun setMarketAdapter(){
+        marketListAdapter = MarketAdapter()
+        binding.rvMarketList.adapter = marketListAdapter
+        binding.rvMarketList.layoutManager = GridLayoutManager(requireContext(), 2)
+        Log.d("************SetAdapter",viewModel.itemList.value.toString())
+    }
+
+    private fun setListObserver(){
+        viewModel.itemList.observe(viewLifecycleOwner) {itemList ->
+            with(binding.rvMarketList.adapter as MarketAdapter) {
+                setList(itemList)
+            }
         }
     }
 }
